dbf4d8ae14d928d9e23c01e1efea9d86142eeb73
Fred Myers <fred@fred-myers.com>
2013-11-12 15:59:40 -0500

    Initial Commit

---
+++ CoolViewTransitions/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  CoolViewTransitions
+//
+//  Created by Fredrick Myers on 11/12/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoolViewTransitions/FMViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMViewController.h
+//  CoolViewTransitions
+//
+//  Created by Fredrick Myers on 11/12/13.
+//  Copyright (c) 2013 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMViewController : UIViewController
+
+@end
